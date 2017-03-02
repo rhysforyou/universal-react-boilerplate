159b018c31dac84b13690495a946f184bb92004d
rpowelll <rhys@rpowell.me>
2017-03-02 22:07:27 +1100

    basic express server

--- src/server/server.js
+++ src/server/server.js
@@ -3,21 +3,21 @@ import path from 'path'
 
 const app = express()
 
-// Figure out what scripts we need to load
+// The JS files we should embed in our template
 let scriptPaths
 
 if (process.env.NODE_ENV === 'production') {
   const sources = ['manifest.js', 'vendor.js', 'bundle.js']
   const manifest = require('./public/manifest.json')
   scriptPaths = sources
-    .map(source => `/public/${manifest[source]}`)
+    .map(source => `/${manifest[source]}`)
 } else {
   const webpack = require('webpack')
   const config = require('../../webpack.config.dev')
   const compiler = webpack(config)
 
   app.use(require('webpack-dev-middleware')(compiler))
-  scriptPaths = ['bundle.js']
+  scriptPaths = ['/bundle.js']
 }
 
 // Set up Handlebars to serve our index template
@@ -25,7 +25,7 @@ app.set('views', path.resolve(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
 // Serve static webpack assets in production
-app.use('/public', express.static(path.resolve(__dirname, 'public')))
+app.use('/', express.static(path.resolve(__dirname, 'public')))
 
 app.get('*', (req, res) => {
   res.render('index', { scriptPaths })
