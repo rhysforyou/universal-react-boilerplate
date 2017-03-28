ecc6464722e22f6cbf3fa36dbe42eba8c42b0198
Rhys Powell <rhys@rpowell.me>
2017-03-28 18:21:59 +1100

    better NavigationLink tests

--- src/components/__tests__/NavigationLink.test.js
+++ src/components/__tests__/NavigationLink.test.js
@@ -1,16 +1,16 @@
 /* eslint-env jest */
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import { StaticRouter } from 'react-router-dom'
 import NavigationLink from '../NavigationLink'
 
-const setup = (propOverrides) => {
+const setup = (propOverrides, render = shallow) => {
   const props = Object.assign({
     to: '/test',
     location: '/'
   }, propOverrides)
 
-  const router = shallow(
+  const router = render(
     <StaticRouter location={props.location} context={{}}>
       <NavigationLink {...props} />
     </StaticRouter>
@@ -20,11 +20,19 @@ const setup = (propOverrides) => {
   return {
     props,
     wrapper,
-    router
+    router,
+    link: router.find('a')
   }
 }
 
 it('renders correctly', () => {
   const { wrapper } = setup()
   expect(wrapper).toMatchSnapshot()
+})
+
+it('includes a link to the specified URL', () => {
+  const to = '/a/test/url'
+  const { link } = setup({ to }, mount)
+
+  expect(link.prop('href')).toBe(to)
 })
