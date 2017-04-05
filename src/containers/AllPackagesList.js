dee4b499e27d5b2f6a6bb8587cc2d6c8711ccb72
Rhys Powell <rhys@rpowell.me>
2017-04-05 12:03:16 +1000

    better Reselect library definition

--- src/containers/AllPackagesList.js
+++ src/containers/AllPackagesList.js
@@ -5,8 +5,13 @@ import { searchPackagesRequested } from '../actions/packages'
 import PackageList from '../components/PackageList'
 
 import type { MapStateToProps, MapDispatchToProps } from 'react-redux'
+import type { Selector as ReselectSelector } from 'reselect'
 import type { Package } from '../actions/types'
+import type { Search } from '../reducers/searches'
+import type { PackagesState } from '../reducers/packages'
 import type { State } from '../reducers/demoApp'
+
+type Selector<Result> = ReselectSelector<State, void, Result>;
 
 type StateProps = { packages: Array<Package> }
 
@@ -16,15 +21,15 @@ type DispatchProps = {
 
 type OwnProps = {}
 
-const reactSearchSelector = state => state.searches.react || {}
-const packagesSelector = state => state.entities.packages
+const reactSearchSelector: Selector<Search> = state => state.searches.react || { query: 'react', status: 'loading' }
+const packagesSelector: Selector<PackagesState> = state => state.entities.packages
 
-const reactSearchPackageIdsSelector = createSelector(
+const reactSearchPackageIdsSelector: Selector<Array<string>> = createSelector(
   reactSearchSelector,
-  search => search.status === 'loaded' ? search.packages : []
+  search => (search.status && search.status === 'loaded') ? search.packages : []
 )
 
-const reactSearchPackagesSelector = createSelector(
+const reactSearchPackagesSelector: Selector<Array<Package>> = createSelector(
   reactSearchPackageIdsSelector,
   packagesSelector,
   (ids, packages) => ids.map(id => packages[id])
